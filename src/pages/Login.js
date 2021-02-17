9dcb25679d6d7f9b4c64374bf44566a52bb5d0e3
tombetthauser <tombetthauser@gmail.com>
2021-02-17 15:38:21 -0800

    Get signup page fully styled and wrapper mobile friendly

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,9 +8,7 @@ function Login() {
       <div className="login-wrapper">
         <h2 className="login-h2">Log In</h2>
         <form className="login-form" onSubmit={e => e.preventDefault()}>
-          <label className="login-form_label">username</label>
-          <input className="login-form__input" type="text" name="username" placeholder="Email Address" required />
-          <label className="login-form_label">password</label>
+          <input className="login-form__input" type="text" name="email" placeholder="Email Address" required />
           <input className="login-form__input" name="password" type="password" placeholder="Password" required />
           <button className="login-form__submit" type="submit">Log In</button>
         </form>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,14 +4,16 @@ import Links from '../components/Links.js';
 function Signup() {
   return (
     <div>
-      <h1 className="login-h1">Sign Up</h1>
-      <form className="form" onSubmit={e => e.preventDefault()}>
-        <label className="form_label">username</label>
-        <input className="form__input" type="text" name="username" placeholder="username" required />
-        <label className="form_label">password</label>
-        <input className="form__input" name="password" type="password" placeholder="password" required />
-        <button className="form__submit" type="submit">Sign Up</button>
-      </form>
+      <h1 className="login-h1">ARTHOLE</h1>
+      <div className="login-wrapper">
+        <h2 className="login-h2">Sign Up</h2>
+        <form className="login-form" onSubmit={e => e.preventDefault()}>
+          <input className="login-form__input" type="text" name="username" placeholder="Full Name" required />
+          <input className="login-form__input" type="text" name="email" placeholder="Email Address" required />
+          <input className="login-form__input" name="password" type="password" placeholder="Password" required />
+          <button className="login-form__submit" type="submit">Sign Up</button>
+        </form>
+      </div>
       <Links />
     </div>
   )
