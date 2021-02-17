2bd514f3584e8f446494791116e2d1dafdbaeec3
tombetthauser <tombetthauser@gmail.com>
2021-02-17 15:19:43 -0800

    Get login page fully styled

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,14 +4,17 @@ import Links from '../components/Links.js';
 function Login() {
   return (
     <div>
-      <h1 className="login-h1">Log In</h1>
-      <form className="login-form" onSubmit={e => e.preventDefault()}>
-        <label className="login-form_label">username</label>
-        <input className="login-form__input" type="text" name="username" placeholder="username" required />
-        <label className="login-form_label">password</label>
-        <input className="login-form__input" name="password" type="password" placeholder="password" required />
-        <button className="login-form__submit" type="submit">Log In</button>
-      </form>
+      <h1 className="login-h1">ARTHOLE</h1>
+      <div className="login-wrapper">
+        <h2 className="login-h2">Log In</h2>
+        <form className="login-form" onSubmit={e => e.preventDefault()}>
+          <label className="login-form_label">username</label>
+          <input className="login-form__input" type="text" name="username" placeholder="Email Address" required />
+          <label className="login-form_label">password</label>
+          <input className="login-form__input" name="password" type="password" placeholder="Password" required />
+          <button className="login-form__submit" type="submit">Log In</button>
+        </form>
+      </div>
       <Links />
     </div>
   )
